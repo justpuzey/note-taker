faa3b1ac0dd78fbe58840d28a70abd27e9160aeb
Justin Puzey <justpuzey@gmail.com>
2021-05-09 15:37:38 -0400

    add note id and delete function

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 
+// import { v4 as idCreator } from 'uuid';
+const idCreator = require('uuid');
+
 //Data Source
-const { notes } = require('./db/db');
+let { notes } = require('./db/db');
+notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8'));
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -20,17 +24,37 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-  const notes = fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8')
-  res.json(JSON.parse(notes));
+  res.json(notes);
 });
 
+//Post new note
 app.post('/api/notes', (req, res) => {
-  const note = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8'));
-  note.push(req.body)
-  console.log(note)
-  fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(note))
+
+
+  const { title, text } = req.body
+  const newID = idCreator.v4()
+
+  notes.push({ title, text, id: newID })
+
+  console.log(notes)
+  fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes))
   res.send('File Received')
 })
+
+// Retrieves a note with specific id
+app.get("/api/notes/:id", (req, res) => {
+  res.json(notes[req.params.id]);
+});
+
+// Deletes a note with specific id
+app.delete("/api/notes/:id", function (req, res) {
+  notes.splice(notes.indexOf(req.params.id), 1);
+  console.log(notes.indexOf(req.params.id))
+  fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes))
+
+  console.log("Deleted note with id " + req.params.id);
+  location.reload()
+});
 
 //Listen 
 app.listen(3001, () => {
